c33ced4bc5a276cccb4e2221975453b5d334cc75
Freshield <zxdsw199182@gmail.com>
2016-05-12 13:50:53 -0400

    test environment

---
+++ Preparing_test1/src/foo/class1.java
@@ -0,0 +1,12 @@
+package foo;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("233");
+		System.out.println("lol");
+
+	}
+
+}
