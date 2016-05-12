1596f1be880845fcc87e1deb0e6804af843783a6
Freshield <zxdsw199182@gmail.com>
2016-05-12 15:39:12 -0400

    normal type

---
+++ test1/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by FRESHIELD on 2016/5/12.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        int a;
+        a = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        int v3 = scanner.nextInt();
+        //String v4 = scanner.nextLine();
+        int v4 = scanner.nextInt();
+        System.out.println(v3+v4);
+
+    }
+}
