e8b16d9a7822470335af4d4b6379dad7e6c77308
Freshield <zxdsw199182@gmail.com>
2016-05-12 21:30:35 -0400

    done question3

--- Assignment/Assignment1/src/Question3.java
+++ Assignment/Assignment1/src/Question3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by FRESHIELD on 2016/5/12.
  */
@@ -5,6 +7,21 @@ public class Question3 {
 
     public static void main(String[] args) {
 
+        System.out.print("Please input one number between 0 to 9999: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        String fill = new String();
+        if (input / 10 == 0) {
+            fill = "000";
+        }else if (input / 100 == 0) {
+            fill = "00";
+        }else if (input / 1000 == 0) {
+            fill = "0";
+        }else {
+            fill = "";
+        }
+
+        System.out.println(fill+input);
 
     }
 }
