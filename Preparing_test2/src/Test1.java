285ade12c4c8e64574361f9fc06d2b6a1ff95914
Freshield <zxdsw199182@gmail.com>
2016-05-19 15:14:20 -0400

    test three condition if

---
+++ Preparing_test2/src/Test1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by FRESHIELD on 2016/5/19.
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+
+        x *= x < 0 ? -1 : 1;
+        
+    }
+}
