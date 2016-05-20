87cabe17c82bc0e971f7b7427773ad914eb3b8c6
Freshield <zxdsw199182@gmail.com>
2016-05-19 21:32:02 -0400

    change while use for

--- Preparing_test2/src/Test2.java
+++ Preparing_test2/src/Test2.java
@@ -22,5 +22,7 @@ public class Test2 {
         for (int i = array.length-1; i >= 0; i--) {
             System.out.println(array[i]);
         }
+
+
     }
 }
---
+++ Preparing_test2/src/Test3.java
@@ -0,0 +1,13 @@
+/**
+ * Created by FRESHIELD on 2016/5/19.
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
