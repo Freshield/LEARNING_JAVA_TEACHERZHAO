7f71888669fb807a0669c342395250d979423e5c
Freshield <zxdsw199182@gmail.com>
2016-05-21 16:22:02 -0400

    done right up matrix

---
+++ Assignment/Assignment2/Matrix/Matrix/Source/src/Matrix.java
@@ -0,0 +1,234 @@
+/**
+ * Created by Zhaozhe on 5/19/16.
+ */
+public class Matrix {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int[][] sourceMatrix(){
+        int[][] matrix = {
+                {0, 1, 2, 3},
+                {1, 0, 0, 3},
+                {1, 1, 1, 3},
+                {1, 2, 3, 0}
+        };
+        return matrix;
+    }
+
+    /*
+     *  .   要求
+     *      把数据转换为图形字符输出
+     *      0 -> .
+     *      1 -> X
+     *      2 -> @
+     *      3 -> #
+     *      4 -> O
+     *      5 -> %
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      . X @ #
+     *      X . . #
+     *      X X X #
+     *      X @ # .
+     */
+    public static void printIcons(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据里是 0 的地方, 随机补充 1 - 5 的数字
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 1 2 3
+     *      1 5 4 3
+     *      1 1 1 3
+     *      1 2 3 2
+     */
+    public static void fill(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      向右侧翻滚一格, 最右侧变为最左侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      3 0 1 2
+     *      3 1 0 0
+     *      3 1 1 1
+     *      0 1 2 3
+     */
+    public static void rollToRight(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      向上侧翻滚一格, 最上侧变为最下侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      0 1 2 3
+     */
+    public static void rollToTop(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把非 0 数据推向右侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      0 1 2 3
+     *      0 0 1 3
+     *      1 1 1 3
+     *      0 1 2 3
+     */
+    public static void pushToRight(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把非 0 数据推向上侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      0 1 2 3
+     *      0 0 1 3
+     *      1 1 1 3
+     *      0 1 2 3
+     */
+    public static void pushToTop(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据横向翻转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      3 2 1 0
+     *      3 0 0 1
+     *      3 1 1 1
+     *      0 3 2 1
+     */
+    public static void flipHorizontally(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据纵向翻转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 2 3 0
+     *      1 1 1 3
+     *      1 0 0 3
+     *      0 1 2 3
+     */
+    public static void flipVertically(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据向右旋转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 1 1 0
+     *      2 1 0 1
+     *      3 1 0 2
+     *      0 3 3 3
+     */
+    public static void rotateRight(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据向左旋转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      3 3 3 0
+     *      2 0 1 3
+     *      1 0 1 2
+     *      0 1 1 1
+     */
+    public static void rotateLeft(int[][] matrix) {
+
+    }
+}
---
+++ Assignment/Assignment2/src/Matrix.java
@@ -0,0 +1,291 @@
+/**
+ * Created by Zhaozhe on 5/19/16.
+ */
+public class Matrix {
+    public static void main(String[] args) {
+//        printIcons(sourceMatrix());
+//        fill(sourceMatrix());
+//        rollToRight(sourceMatrix());
+        rollToTop(sourceMatrix());
+    }
+
+
+    public static int[][] sourceMatrix(){
+        int[][] matrix = {
+                {0, 1, 2, 3},
+                {1, 0, 0, 3},
+                {1, 1, 1, 3},
+                {1, 2, 3, 0}
+        };
+        return matrix;
+    }
+
+    /*
+     *  .   要求
+     *      把数据转换为图形字符输出
+     *      0 -> .
+     *      1 -> X
+     *      2 -> @
+     *      3 -> #
+     *      4 -> O
+     *      5 -> %
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      . X @ #
+     *      X . . #
+     *      X X X #
+     *      X @ # .
+     */
+    public static void printIcons(int[][] matrix) {
+        String[] icons = {".","X","@","#","O","%"};
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(icons[matrix[i][j]]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /*
+     *  .   要求
+     *      把数据里是 0 的地方, 随机补充 1 - 5 的数字
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 1 2 3
+     *      1 5 4 3
+     *      1 1 1 3
+     *      1 2 3 2
+     */
+    public static void fill(int[][] matrix) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (matrix[i][j] == 0) {
+                    System.out.print((int)(Math.random()*5)+1);
+                } else {
+                    System.out.print(matrix[i][j]);
+                }
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /*
+     *  .   要求
+     *      向右侧翻滚一格, 最右侧变为最左侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      3 0 1 2
+     *      3 1 0 0
+     *      3 1 1 1
+     *      0 1 2 3
+     */
+    public static void rollToRight(int[][] matrix) {
+
+        for (int i = 0; i < 4; i++) {
+            int temp = matrix[3][i];
+            for (int j = 0; j < 4; j++) {
+                if ((3 - j) != 0) {
+                    matrix[3 - j][i] = matrix[3 - j - 1][i];
+                } else {
+                    matrix[3 - j][i] = temp;
+                }
+
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /*
+     *  .   要求
+     *      向上侧翻滚一格, 最上侧变为最下侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      0 1 2 3
+     */
+    public static void rollToTop(int[][] matrix) {
+
+        for (int i = 0; i < 4; i++) {
+            int temp = matrix[i][3];
+            for (int j = 0; j < 4; j++) {
+                if ((3 - j) != 0) {
+                    matrix[i][3 - j] = matrix[i][3 - j - 1];
+                } else {
+                    matrix[i][3 - j] = temp;
+                }
+
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    /*
+     *  .   要求
+     *      把非 0 数据推向右侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      0 1 2 3
+     *      0 0 1 3
+     *      1 1 1 3
+     *      0 1 2 3
+     */
+    public static void pushToRight(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把非 0 数据推向上侧
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      0 1 2 3
+     *      0 0 1 3
+     *      1 1 1 3
+     *      0 1 2 3
+     */
+    public static void pushToTop(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据横向翻转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      3 2 1 0
+     *      3 0 0 1
+     *      3 1 1 1
+     *      0 3 2 1
+     */
+    public static void flipHorizontally(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据纵向翻转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 2 3 0
+     *      1 1 1 3
+     *      1 0 0 3
+     *      0 1 2 3
+     */
+    public static void flipVertically(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据向右旋转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      1 1 1 0
+     *      2 1 0 1
+     *      3 1 0 2
+     *      0 3 3 3
+     */
+    public static void rotateRight(int[][] matrix) {
+
+    }
+
+    /*
+     *  .   要求
+     *      把数据向左旋转
+     *      并输出
+     *
+     *  .   效果
+     *      matrix
+     *      0 1 2 3
+     *      1 0 0 3
+     *      1 1 1 3
+     *      1 2 3 0
+     *      <<
+     *      3 3 3 0
+     *      2 0 1 3
+     *      1 0 1 2
+     *      0 1 1 1
+     */
+    public static void rotateLeft(int[][] matrix) {
+
+    }
+}
