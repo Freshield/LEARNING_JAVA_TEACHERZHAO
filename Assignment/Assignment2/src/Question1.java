e959be27767141830ecfc58ae16fe09ff487e8df
Freshield <zxdsw199182@gmail.com>
2016-05-21 14:40:26 -0400

    done question1

--- Assignment/Assignment2/src/Question1.java
+++ Assignment/Assignment2/src/Question1.java
@@ -6,16 +6,18 @@ import java.util.Scanner;
 public class Question1 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a number between 0 to 9999: ");
         int input = scanner.nextInt();
 
-        do {
-            System.out.println("Please input a number between 0 to 9999: ");
-            input = scanner.nextInt();
+        int[] units = new int[4];
+        int divesor = 1000;
+        int counter = 4;
 
-            int[] units = new int[4];
-            int divesor = 1000;
-            int counter = 4;
+        if (input == 0) {
+            System.out.println(0);
+        } else {
 
             //figure the digit number
             for (int i = 0; i < units.length; i++) {
@@ -26,19 +28,21 @@ public class Question1 {
                 counter--;
                 divesor /= 10;
             }
-            System.out.println(counter);
 
-        }while (input != 233);
+            divesor = 1;
 
+            for (int i = 1; i < counter; i++) {
+                divesor *= 10;
+            }
 
-//        divesor = 1;
-//        for (int i = 1; i < counter; i++) {
-//            divesor *= 10;
-//        }
-
-        //get the units
-//        for (int i = 0; i < counter; i++) {
-//
-//        }
+            //get the units and output
+            for (int i = 0; i < counter; i++) {
+                units[i] = input / divesor;
+                input %= divesor;
+                divesor /= 10;
+                System.out.println(units[i]);
+            }
+            
+        }
     }
 }
