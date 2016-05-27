53ee5c2fc1f481c1a9d1eea2867e1c3771c37461
Freshield <zxdsw199182@gmail.com>
2016-05-27 15:02:56 -0400

    test3

---
+++ Preparing_test3/src/test3.java
@@ -0,0 +1,18 @@
+/**
+ * Created by FRESHIELD on 2016/5/27.
+ */
+public class test3 {
+    public static void main(String[] args) {
+        new test3().run();
+    }
+
+    public void run() {
+        System.out.println("1");
+        f();
+        System.out.println("2");
+    }
+
+    public void f() {
+        System.out.println("Hello");
+    }
+}
