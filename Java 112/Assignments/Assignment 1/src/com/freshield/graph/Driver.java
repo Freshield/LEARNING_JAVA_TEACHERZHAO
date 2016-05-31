8e27c91dc625faf145b74a960f9dba7fb5aa5355
Freshield <zxdsw199182@gmail.com>
2016-05-31 19:47:09 -0400

    done point

---
+++ Java 112/Assignments/Assignment 1/src/com/freshield/graph/Driver.java
@@ -0,0 +1,10 @@
+package com.freshield.graph;
+
+/**
+ * Created by FRESHIELD on 2016/5/31.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        new Graph().run();
+    }
+}
---
+++ Java 112/Assignments/Assignment 1/src/com/freshield/graph/Graph.java
@@ -0,0 +1,20 @@
+package com.freshield.graph;
+
+/**
+ * Created by FRESHIELD on 2016/5/31.
+ */
+public class Graph {
+
+    public void run() {
+
+        Point point = new Point();
+        point.setX(-5);
+        point.setY(-3);
+        point.unitize();
+        System.out.println(point.description());
+    }
+
+    public void run2() {
+
+    }
+}
