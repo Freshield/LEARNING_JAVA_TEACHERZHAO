3c39824431ba24d30479afc3df658c7aed6c0ee1
Freshield <zxdsw199182@gmail.com>
2016-06-07 16:49:50 -0400

    test foreach

--- test4/src/com/freshield/collection/system/Driver.java
+++ test4/src/com/freshield/collection/system/Driver.java
@@ -1,10 +1,28 @@
 package com.freshield.collection.system;
 
+import java.util.LinkedList;
+
 /**
  * Created by FRESHIELD on 2016/6/7.
  */
 public class Driver {
     public static void main(String[] args) {
-        
+        LinkedList<String> names = new LinkedList<String>();
+        names.add("Xiaoming");
+        names.add("XiaoHong");
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        for(String name:names) {
+            System.out.println(name);
+        }
+
+        int[] numbers = {2,3,4};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
     }
 }
