3172232ab18744759aac9098ce4ab9da9c163e84
Freshield <zxdsw199182@gmail.com>
2016-06-09 15:05:14 -0400

    override

--- test5/src/com/freshield/inheritance/Human.java
+++ test5/src/com/freshield/inheritance/Human.java
@@ -4,8 +4,12 @@ package com.freshield.inheritance;
  * Created by FRESHIELD on 2016/6/9.
  */
 public class Human extends Animal {
-    public int balance;
+    public double balance;
     public void work(){
         System.out.println("Human work");
+    }
+    public void eat(){
+        System.out.println("Human eat");
+        super.eat();
     }
 }
